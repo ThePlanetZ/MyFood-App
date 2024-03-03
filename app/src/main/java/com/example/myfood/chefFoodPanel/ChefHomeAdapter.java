872127ff099e13e31b47cfe131381cad049db046
zikrag <kaoukab.zakariae@ump.ac.ma>
2024-03-03 22:37:53 +0100

    Chef home Recyclerview done

--- app/src/main/java/com/example/myfood/chefFoodPanel/ChefHomeAdapter.java
+++ app/src/main/java/com/example/myfood/chefFoodPanel/ChefHomeAdapter.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.myfood.R;
 import com.example.myfood.UpdateDishModel;
 
@@ -20,7 +22,7 @@ public class ChefHomeAdapter extends RecyclerView.Adapter<ChefHomeAdapter.ViewHo
     private Context mcont;
     private List<UpdateDishModel> updateDishModelList;
 
-    public ChefHomeAdapter(Context context , List<UpdateDishModel>updateDishModelList){
+    public ChefHomeAdapter(Context context, List<UpdateDishModel> updateDishModelList) {
         this.updateDishModelList = updateDishModelList;
         this.mcont = context;
     }
@@ -28,7 +30,7 @@ public class ChefHomeAdapter extends RecyclerView.Adapter<ChefHomeAdapter.ViewHo
     @NonNull
     @Override
     public ChefHomeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mcont).inflate(R.layout.chefmenu_update_delete,parent,false);
+        View view = LayoutInflater.from(mcont).inflate(R.layout.chefmenu_update_delete, parent, false);
         return new ChefHomeAdapter.ViewHolder(view);
     }
 
@@ -36,17 +38,24 @@ public class ChefHomeAdapter extends RecyclerView.Adapter<ChefHomeAdapter.ViewHo
     public void onBindViewHolder(@NonNull ChefHomeAdapter.ViewHolder holder, int position) {
 
         final UpdateDishModel updateDishModel = updateDishModelList.get(position);
-        holder.dishes.setText(updateDishModel.getDishes());
-        updateDishModel.getRandomUID();
+
+        // Load dish image using Glide
+        Glide.with(mcont)
+                .load(updateDishModel.getImageURL()) // Assuming getImageURL() returns the URL of the dish image
+                .placeholder(R.drawable.placeholder) // Placeholder image while loading
+                .error(R.drawable.error1) // Image to display in case of an error
+                .into(holder.dishImageView);
+
+        holder.dishName.setText(updateDishModel.getDishes());
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(mcont,UpdateDelete_Dish.class);
-                intent.putExtra("updatedeletedish",updateDishModel.getRandomUID());
+                Intent intent = new Intent(mcont, UpdateDelete_Dish.class);
+                intent.putExtra("updatedeletedish", updateDishModel.getRandomUID());
                 mcont.startActivity(intent);
             }
         });
-
     }
 
     @Override
@@ -54,12 +63,14 @@ public class ChefHomeAdapter extends RecyclerView.Adapter<ChefHomeAdapter.ViewHo
         return updateDishModelList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView dishes;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView dishImageView;
+        TextView dishName;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            dishes = itemView.findViewById(R.id.dish_name);
+            dishImageView = itemView.findViewById(R.id.dish_image);
+            dishName = itemView.findViewById(R.id.dish_name);
         }
     }
 }
